cf94f0f20cb2df2fadab4539ca83377d37554fa7
winner <921876203@qq.com>
2025-06-28 15:25:58 +0800

    fix:修改项目部署到githubpage后_plugin-vue_export-helper访问404的问题

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import { defineConfig } from "vite";
 import Markdown from "vite-plugin-md";
 const pathResolve = (dir: string) => resolve(__dirname, dir);
 
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
@@ -27,6 +29,26 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": pathResolve("./src"), //配置别名简称
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: "index.html",
+      },
+      // 静态资源分类打包
+      output: {
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : "";
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        },
+      },
     },
   },
 });
